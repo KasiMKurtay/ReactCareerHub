b88d97e7f556c2471263d9c4b38d75581137022f
KasiMKurtay <kasmkurtay@outlook.com>
2025-03-02 23:35:31 +0300

    Homepage added and used as a JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
+import { Route, 
+  createBrowserRouter,
+   createRoutesFromElements,
+    RouterProvider } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import MainLayout from "./layouts/MainLayout"
+import JobsPage from "./pages/JobsPage"
+import NotFoundPage from "./pages/NotFoundPage"
 
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import React from 'react'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path="/" element={<MainLayout/>}>
+  <Route index element={< HomePage />} />
+  <Route path="/jobs" element={<JobsPage/>}/>
+  <Route path="*" element={<NotFoundPage/>}/>
+  </Route>
+  )
+)
 
 const App = () => {
-  return (
-    <>
-    <Navbar />
-     <Hero />
-    <HomeCards />
-    <JobListings/>
-    <ViewAllJobs />
-    </>
-  )
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
